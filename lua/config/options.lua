d8115afb0b6f12e03c427b24255fe74c8dd372d6
S-Axhwin <ashwin.sathiyamoorthy.d@gmail.com>
2026-01-17 01:45:22 +0530

    added github neotree

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,11 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.opt.showtabline = 0
+
+vim.opt.list = false
+
+vim.opt.list = false
 vim.g.which_key_enabled = false
 vim.diagnostic.config({
   virtual_text = false,
--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,6 +1,31 @@
 return {
 
   { "akinsho/bufferline.nvim", enabled = false },
+
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    enabled = false,
+  },
+
+  {
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      opts = {
+        enabled = false, -- Disables the plugin entirely
+        -- or configure specific features:
+        -- show_trailing_blankline = false,
+        -- show_first_indent_line = false,
+      },
+    },
+  },
+  {
+    "nvim-mini/mini.indentscope",
+    enabled = false,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
